f4adae4a2f404556a89781b76038c97053438c5f
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2021-03-05 13:07:15 -0300

    update data

--- lib/repository/data.dart
+++ lib/repository/data.dart
@@ -79,4 +79,102 @@ final guardians1 = MCU(
     "https://i.pinimg.com/originals/d0/74/a2/d074a2b6cce2eb7d392a101ebcaf7b8a.jpg",
     "https://i.pinimg.com/originals/d0/74/a2/d074a2b6cce2eb7d392a101ebcaf7b8a.jpg");
 
-final List<MCU> list = [captainOne,captainMarvel, ironMan, ironMan2, hulk, thor, avengers, ironMan3, thor2, captain2,guardians1];
+final guardians2 = MCU(
+    "Guardiões da Galáxia Vol. 2",
+    "https://www.youtube.com/watch?v=4-i8nTNSQFI",
+    "Esta sequência começa alguns meses após o original. Peter Quill está se apaixonando cada vez mais por Gamora, de Zoe Saldana. E ele é forçado a confrontar os mistérios de seu passado quando fica cara a cara com uma entidade divina conhecida como Ego, interpretado por Kurt Russell. Parte do desafio de colocar esses filmes do Guardian of the Galaxy na linha do tempo MCU é que eles parecem separados e todos acontecem no espaço sideral",
+    "https://i.pinimg.com/originals/a3/c1/43/a3c143aa0251da8259c447e2f87a52af.jpg",
+    "https://i.pinimg.com/originals/a3/c1/43/a3c143aa0251da8259c447e2f87a52af.jpg");
+
+final avenger2 = MCU(
+    "Vingadores: Era de Ultron",
+    "https://www.youtube.com/watch?v=lGO6FcnZ0ek",
+    "Os Vingadores se reúnem para enfrentar um erro criado por Tony Stark e Bruce Banner: o AI conhecido como Ultron, dublado por James Spader. Se uma única versão robótica de Ultron for deixada de pé, ele pode continuar lutando contra os Vingadores. Este filme também apresenta novos Vingadores: The Scarlet Witch (interpretada por Elizabeth Olson), Quicksilver (interpretada por Aaron Taylor-Johnson) e Vision (interpretada por Paul Bettany).",
+    "https://i.pinimg.com/originals/0e/21/00/0e2100e75abc9d62f6a9ce59618a9e18.jpg",
+    "https://i.pinimg.com/originals/0e/21/00/0e2100e75abc9d62f6a9ce59618a9e18.jpg");
+    
+final ant = MCU(
+    "Homem-formiga",
+    "https://www.youtube.com/watch?v=wITUolzfkwI",
+    "O Homem-Formiga é estrelado por Paul Rudd como um ladrão de gatos recrutado por Hank Pym de Michael Douglas para revelar o processo do Homem-Formiga a fim de impedir que a tecnologia se torne uma arma. O ex-prodígio de Pym (interpretado por Corey Stoll) recriou a tecnologia na forma de um paletó amarelo, e o Homem-Formiga deve lutar contra ele e salvar o dia na menor escala imaginável.",
+    "https://i.pinimg.com/originals/f3/cc/87/f3cc87bc68b0cdad3336a3bcd6b2722c.jpg",
+    "https://i.pinimg.com/originals/f3/cc/87/f3cc87bc68b0cdad3336a3bcd6b2722c.jpg");
+
+final captain3 = MCU(
+    "Capitão América: Guerra Civil",
+    "https://www.youtube.com/watch?v=3p1d_6_ocEE",
+    "Embora seja um filme do Capitão América, Guerra Civil apresenta quase todos os Vingadores, ao mesmo tempo em que adiciona mais dois pesos pesados à programação: Pantera Negra de Chadwick Boseman e Homem-Aranha de Tom Holland. Infelizmente, os Vingadores estão divididos em diferentes facções devido ao Capitão América querer salvar seu amigo Bucky Barnes, que parece ser o responsável pelo bombardeio de uma sessão da ONU.",
+    "https://i.pinimg.com/originals/fc/f5/be/fcf5beae71c46f8e24000d511bbf38f2.jpg",
+    "https://i.pinimg.com/originals/fc/f5/be/fcf5beae71c46f8e24000d511bbf38f2.jpg");
+
+final spider = MCU(
+    "Homem-Aranha: Homicoming",
+    "https://www.youtube.com/watch?v=2x-2iYxgMFU",
+    "O lançador de teias faz sua estréia solo aqui, onde ele enfrenta Michael Keatons Vulture, um capataz de construção que se tornou um negociante de armas no mercado negro após recuperar a tecnologia da batalha de Nova York no primeiro filme dos Vingadores. Além de tudo isso, Peter Parker também está lidando com todos os problemas usuais que surgem em ser um calouro no ensino médio",
+    "https://i.pinimg.com/736x/6e/e7/28/6ee7287f1096d59277325e70858db9de.jpg",
+    "https://i.pinimg.com/736x/6e/e7/28/6ee7287f1096d59277325e70858db9de.jpg");
+
+final doctor = MCU(
+    "Doutor Estranho",
+    "https://www.youtube.com/watch?v=YUfWrIcX4zw",
+    "Stephen Strange, interpretado por Benedict Cumberbatch, é um cirurgião de classe mundial, e ele sabe disso. Estranho é insuportável para quase todo mundo que tem que lidar com ele até que um trágico acidente o tire do uso de suas mãos. Strange então viaja pelo mundo em busca de uma cura que lhe dê de volta o uso de suas mãos - e ele encontra uma resposta, na forma de alguma magia antiga.",
+    "https://i.pinimg.com/originals/32/67/c6/3267c625725cd242e49519620f79a63b.jpg",
+    "https://i.pinimg.com/originals/32/67/c6/3267c625725cd242e49519620f79a63b.jpg");
+
+final blackPanther = MCU(
+    "Pantera Negra",
+    "https://www.youtube.com/watch?v=wL4a4MafSjQ",
+    "Após o bombardeio da ONU no Capitão América: A Guerra Civil, TChalla deve voltar para casa em Wakanda e ser nomeado rei. Uma vez lá, ele é confrontado com a contínua política de isolamento que ajudou a proteger Wakanda. Ele também enfrenta um erro do passado de seu pai na forma de Eric Killmonger, que é interpretado por Michael B Jordan e pode ser o melhor vilão de qualquer filme da Marvel.",
+    "https://i.pinimg.com/originals/de/0a/5e/de0a5e8f494869109d798a89bedfdcb9.jpg",
+    "https://i.pinimg.com/originals/de/0a/5e/de0a5e8f494869109d798a89bedfdcb9.jpg");
+
+final thor3 = MCU(
+    "Thor: Ragnarok",
+    "https://www.youtube.com/watch?v=UvNnqWLruXA",
+    "O terceiro filme solo de Thor mostra o herói lançado através do espaço após a morte de seu pai e a destruição de seu martelo por sua irmã há muito perdida, Hella, interpretada por Cate Blanchett. Ele se vê preso em lutas de gladiadores contra o Hulk, que não era visto desde que os Vingadores derrotaram Ultron. Juntos, Thor e Hulk se unem a Loki e a Valquíria de Tessa Thompson para enfrentar Hella.",
+    "https://i.pinimg.com/originals/e1/2c/87/e12c87434d1cc1508290f846790bb3d6.jpg",
+    "https://i.pinimg.com/originals/e1/2c/87/e12c87434d1cc1508290f846790bb3d6.jpg");
+
+final ant2 = MCU(
+    "Homem-formiga e a Vespa",
+    "https://www.youtube.com/watch?v=0HGBv7s1Y7E",
+    "Scott Lang está de volta, mas ele foi colocado em prisão domiciliar após seu papel na Guerra Civil e ficar do lado do Capitão América. Ele está afastado de Hank Pym e da filha de Pym, Hope. Mas ele relutantemente concorda em ajudá-los, pensando que Pym pode ser capaz de resgatar a mãe de Hope do Reino Quântico. (Atenção: salve a cena pós-créditos até depois de ver a Guerra do Infinito.)",
+    "https://www.hdwallpapers.in/download/ant_man_and_the_wasp_4k_8k-1440x2560.jpg",
+    "https://www.hdwallpapers.in/download/ant_man_and_the_wasp_4k_8k-1440x2560.jpg");
+
+final black = MCU(
+    "Viúva Negra",
+    "https://www.youtube.com/watch?v=Lk7LPTq0_XY",
+    "A Viúva Negra está finalmente recebendo seu próprio filme autointitulado, embora tenha demorado para chegar aos cinemas devido ao coronavírus.Infelizmente, foi necessária a morte da Viúva Negra no Endgame para ela conseguir um filme independente. Mesmo assim, o próximo filme da Marvel seguirá Natasha Romanov de Scarlett Johansson durante um período de exílio em sua vida. Acontece após os eventos do Capitão América: Guerra Civil, mas antes da Guerra do Infinito e do Snap. Ele está sendo construído como uma prequela para explicar a história da Viúva Negra.",
+    "https://i.pinimg.com/originals/1b/52/7f/1b527f63fc2d4514c7ccac04f3e3b02e.jpg",
+    "https://i.pinimg.com/originals/1b/52/7f/1b527f63fc2d4514c7ccac04f3e3b02e.jpg");
+
+final avenger3 = MCU(
+    "Vingadores: Guerra Infinita",
+    "https://www.youtube.com/watch?v=t_ULBP6V9bg",
+    "Depois de anos orquestrando coisas nos bastidores para encontrar todas as Pedras do Infinito, Thanos (dublado por Josh Brolin) decidiu ir buscá-las ele mesmo. A única coisa que está em seu caminho são os Vingadores, que atualmente estão espalhados por todo o universo. Para aumentar ainda mais as apostas, a única razão de Thanos para buscar o poder das pedras é destruir metade de toda a vida no universo.",
+    "https://i.pinimg.com/736x/56/0d/ea/560dea40dbf5b4e9c250aa89fc67f852.jpg",
+    "https://i.pinimg.com/736x/56/0d/ea/560dea40dbf5b4e9c250aa89fc67f852.jpg");
+
+final avenger4 = MCU(
+    "Vingadores: Ultimato",
+    "https://www.youtube.com/watch?v=g6ng8iy-l0U",
+    "Depois que Thanos tirou metade de toda a vida, deixando o universo em um caos total, os Vingadores devem tentar consertar as coisas. Cinco anos se passam e uma pequena chance surge para eles desfazerem tudo, mas antes que isso possa acontecer, o Capitão América e Tony Stark precisam fazer as pazes e reunir os Vingadores uma última vez.",
+    "https://i.pinimg.com/originals/45/9d/3c/459d3c561170ec10ea04fd009af42638.jpg",
+    "https://i.pinimg.com/originals/45/9d/3c/459d3c561170ec10ea04fd009af42638.jpg");
+
+final wanda = MCU(
+    "WandaVision",
+    "https://www.youtube.com/watch?v=NZxfnJt9vKo",
+    "Wanda Maximoff de Elizabeth Olsen tem sido discretamente um dos personagens mais trágicos do MCU. Ela perdeu sua família em um bombardeio, que a deixou presa em uma pilha de escombros por dias com seu irmão. Seu irmão morreu durante os eventos de Vingadores: Idade de Ultron.Ela finalmente encontrou o amor com a Visão de Paul Bettany, mas foi forçada a matá-lo para evitar que Thanos colocasse suas mãos na Pedra Mental. Ah, e depois de matá-lo, Thanos reverteu o tempo e matou Visão novamente na frente dela. Tudo isso nos levou a WandaVision, o novo show live-action da Disney +, que mostra a personagem tomando conta de uma pequena cidade em New Jersey para fazer uma versão mais feliz de sua própria vida. Claro, outra questão importante é: quando isso ocorre em nossa linha do tempo.",
+    "https://lumiere-a.akamaihd.net/v1/images/p_wandavision_disneyplus_poster03_20118_66028c77.jpeg?region=0%2C0%2C540%2C810",
+    "https://lumiere-a.akamaihd.net/v1/images/p_wandavision_disneyplus_poster03_20118_66028c77.jpeg?region=0%2C0%2C540%2C810");
+
+final spider2 = MCU(
+    "Homem-Aranha: Longe de casa",
+    "https://www.youtube.com/watch?v=6bpZXJgtYww",
+    "Far From Home é o último filme da Marvel e serve como nosso primeiro olhar sobre o MCU pós-Infinity War, quando vemos todos que foram fotografados por Thanos voltando à vida cinco anos depois. O recém-retirado Peter Parker segue para a Europa para uma viagem de campo, mas é surpreendido por Nick Fury e Mysterio de Jake Gyllenhaal, que precisam de sua ajuda contra inimigos conhecidos como Elementais.",
+    "https://i.pinimg.com/originals/61/91/6e/61916e03884583cb287fea579eaeeaab.jpg",
+    "https://i.pinimg.com/originals/61/91/6e/61916e03884583cb287fea579eaeeaab.jpg");
+
+final List<MCU> list = [captainOne,captainMarvel, ironMan, ironMan2, hulk, thor, avengers, ironMan3, thor2, captain2,guardians1, guardians2, avenger2, ant, captain3, spider, doctor,blackPanther, thor3, ant2, black, avenger3, avenger4, wanda, spider2];
