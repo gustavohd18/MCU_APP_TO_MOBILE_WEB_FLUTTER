b8472be0b02c207b92c06969b3431ee29f32b624
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2021-03-05 04:21:26 -0300

    add phase 1 marvel

--- lib/repository/data.dart
+++ lib/repository/data.dart
@@ -23,5 +23,33 @@ final captainMarvel = MCU(
     "https://i.pinimg.com/originals/a0/0a/46/a00a46cf821f276ba158e6b9d6ff4a6d.jpg",
     "https://i.pinimg.com/originals/a0/0a/46/a00a46cf821f276ba158e6b9d6ff4a6d.jpg");
 
+final ironMan2 = MCU(
+    "Homem de Ferro 2 ",
+    "https://www.youtube.com/watch?v=SSQ1TBkP7aE",
+    "O segundo Homem de Ferro continua de onde o primeiro parou: Tony Stark lutando para revelar sua identidade de Homem de Ferro. O governo quer a tecnologia por trás de seu terno, e quando Stark se recusa a entregá-lo, outro fabricante de armas mostra que está disposto a fazer qualquer coisa para colocar as mãos nele. Este filme também apresenta outros Vingadores, a Viúva Negra e a Máquina de Guerra.",
+    "https://i.pinimg.com/originals/3a/f5/48/3af5488ed3d6239f7ec1dd06cb240ef3.jpg",
+    "https://i.pinimg.com/originals/3a/f5/48/3af5488ed3d6239f7ec1dd06cb240ef3.jpg");
 
-final List<MCU> list = [captainOne,captainMarvel, ironMan];
+final hulk = MCU(
+    "O incrível Hulk ",
+    "https://www.youtube.com/watch?v=KIxi16u7zUg",
+    "O Incrível Hulk segue Bruce Banner na fuga do General Thunderbolt Ross e do Exército dos EUA. Percebendo que não pode esperar controlar ou conter o Hulk, Ross decide criar sua própria versão do Hulk usando outro soldado, mas rapidamente perde o controle. O Incrível Hulk é estrelado por Edward Norton, mas Mark Ruffalo o substituiu em 2012 e tem sido o grande homem verde desde então.",
+    "https://i.pinimg.com/736x/1f/20/04/1f20046b49c6e9f07edc7fc07e83f0ad.jpg",
+    "https://i.pinimg.com/736x/1f/20/04/1f20046b49c6e9f07edc7fc07e83f0ad.jpg");
+
+final thor = MCU(
+    "Thor",
+    "https://www.youtube.com/watch?v=LRhJrpQfjlE",
+    "O Deus do raio foi banido de Asgard para a Terra por seu pai Odin, tudo graças aos truques de Loki. Para recuperar seus poderes e controlar seu martelo, Thor, retratado por Chris Hemsworth, deve provar que é digno. Felizmente, ele conhece uma simpática cientista da Terra, interpretada por Natalie Portman, que pode ajudá-lo a acertar as coisas antes que Loki assuma o controle total de Asgard.",
+    "https://i.pinimg.com/originals/72/85/db/7285db2ae63b41fecfc47a9db9a6c43c.jpg",
+    "https://i.pinimg.com/originals/72/85/db/7285db2ae63b41fecfc47a9db9a6c43c.jpg");
+
+final avengers = MCU(
+    "Os Vingadores",
+    "https://www.youtube.com/watch?v=KeNEGtsCWEk",
+    "The Avengers é o culminar da chamada Fase 1 do MCU. Com todos os heróis principais apresentados, o verdadeiro desafio era encontrar algo assustador o suficiente para forçá-los a trabalhar juntos. O poder combinado de Loki, o Tesserato e uma horda de alienígenas invadindo a cidade de Nova York provou ser a combinação que fez esses super-heróis se tornarem amigos na vida e na linha de frente.",
+    "https://i.pinimg.com/originals/41/61/43/416143c9c1d1e0925b88259fe868ee6c.jpg",
+    "https://i.pinimg.com/originals/41/61/43/416143c9c1d1e0925b88259fe868ee6c.jpg");
+
+
+final List<MCU> list = [captainOne,captainMarvel, ironMan, ironMan2, hulk, thor, avengers];
